6562c5e6d7f6315dcf778a8bd839f86b9921e620
Jack Lawson <jackglawson@gmail.com>
2020-11-21 16:03:52 +0000

    v1

---
+++ README.md
@@ -0,0 +1,5 @@
+# A sweep and prune algorithm
+
+For detecting collisions.
+
+https://en.wikipedia.org/wiki/Sweep_and_prune
---
+++ sweep_and_prune.py
@@ -0,0 +1,27 @@
+from typing import List, Tuple
+
+
+def sweep_and_prune(bounds: List[Tuple]) -> List[List]:
+    """Detect collisions between objects in 1-dimension using a sweep and prune algorithm"""
+
+    collisions = [[False] * len(bounds) for _ in range(len(bounds))]
+
+    l = []
+    for i, x in enumerate(bounds):
+        l.append((x[0], "s", i))
+        l.append((x[1], "e", i))
+    l.sort()
+
+    active_particles = set()
+    for t in l:
+        if t[1] == "s":
+            active_particles.add(t[2])
+            for p in active_particles:
+                collisions[p][t[2]] = collisions[t[2]][p] = True
+        elif t[1] == "e":
+            active_particles.remove(t[2])
+
+    return collisions
+
+
+print(sweep_and_prune([(0, 5), (1, 2), (3, 4)]))
